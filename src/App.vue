63b55cc7ef6b4fd62a4191607ecc52dc908f57f4
GitSojin <dfsa356@naver.com>
2024-03-09 04:27:58 +0900

    블로그글 목록_컴포넌트,props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List :blogText="blogText" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import blog from "./assets/blog";
+import List from "./components/List.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { List },
+  data() {
+    return { blogText: blog };
+  },
+};
 </script>
 
 <style>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h5>{{ blogText[0].title }}</h5>
+    <p>Today : {{ blogText[0].date }}</p>
+  </div>
+  <div>
+    <h5>{{ blogText[1].title }}</h5>
+    <p>Today : {{ blogText[1].date }}</p>
+  </div>
+  <div>
+    <h5>{{ blogText[2].title }}</h5>
+    <p>Today : {{ blogText[2].date }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    blogText: Object,
+  },
+};
+</script>
+<style></style>
